f96f71c3929e3e7c1f0c5781cf97767aeeb03ffe
Mona Lisa <giatran1222@gmail.com>
2022-12-20 01:08:48 -0800

    finished demo resume

--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -2,8 +2,73 @@ import React from "react";
 
 export const Resume = () => {
     return (
-        <section>
-            <h1>Resume</h1>
-        </section>
+        <main id="main">
+            <section id="resume" className="resume">
+                <div className="container  my-3">
+                    <div className="section-title">
+                        <h2>Resume</h2>
+                        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <h3 className="resume-title my-3 mx-2">Summary</h3>
+                                <div className="resume-item pb-0">
+                                    <h4>Gia Tran</h4>
+                                    <p><em>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem a eos officiis ducimus maiores, ab aspernatur, alias libero nemo reprehenderit molestias eligendi, quisquam eveniet fugit est commodi veritatis delectus repellat.</em></p>
+                                    <p></p>
+                                    <ul>
+                                        <li>City, State</li>
+                                        <li>(123) 456-7891</li>
+                                        <li>gia.tran@example.com</li>
+                                        </ul>
+                                        <p></p>
+                                        </div>
+                                        <h3 className="resume-title">Education</h3>
+                                        <div className="resume-item">
+                                            <h4>Degree &amp; Name</h4>
+                                            <h5>2022 - 2022</h5>
+                                            <p><em>University of Minnesota</em></p>
+                                            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+                                            </div>
+                                            <div className="resume-item">
+                                                <h4>Bachelor of Arts &amp; Degree</h4>
+                                                <h5>2015 - 2019</h5>
+                                                <p><em>University of California</em></p>
+                                                <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+                                                </div>
+                                                </div>
+                                                <div className="col-lg-6">
+                                                    <h3 className="resume-title my-3 mx-2">Professional Experience</h3>
+                                                    <div className="resume-item">
+                                                        <h4>Position Title</h4>
+                                                        <h5>Year</h5>
+                                                        <p><em>Location </em></p>
+                                                        <p></p>
+                                                        <ul>
+                                                            <li>task1</li>
+                                                            <li>task2</li>
+                                                            <li>task3</li>
+                                                            <li>task4</li>
+                                                            </ul>
+                                                            <p></p>
+                                                            </div>
+                                                            <div className="resume-item">
+                                                                <h4>Position Title</h4>
+                                                                <h5>Year</h5>
+                                                                <p><em>Location</em></p>
+                                                                <p></p>
+                                                                <ul>
+                                                                    <li>task1</li>
+                                                                    <li>task2</li>
+                                                                    <li>task3</li>
+                                                                    <li>task4</li>
+                                                                    </ul>
+                                                                    <p></p>
+                                                                    </div>
+                                                                    </div>
+                                                                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
